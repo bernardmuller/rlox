626c30724d02f348eb380ea9e896d6c75322a137
Bernard <b.mullerjnr@gmail.com>
2026-04-21 09:48:35 +0200

    feat: parse file path arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        return Err("Usage: [Usage]".into());
+    }
+
+    let file_path = &args[1];
+
+    println!("{}", file_path);
+    Ok(())
 }
